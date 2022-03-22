3dec49b6de8ba2e0d011e0937a56dd706baed8b2
Karla Abad <kabad1014@gmail.com>
2022-03-22 10:38:19 -0500

    Front end components
    Added Login, Registration, Nav and About components HTML

---
+++ client/src/components/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Nav from "./Nav";
+
+const About = () => {
+  return (
+    <div>
+      <Nav />
+      <div>
+        <label>Add School</label>
+        <label>Add current city</label>
+        <label>Add hometown</label>
+        <label>Add status</label>
+      </div>
+    </div>
+  );
+};
+
+export default About;
