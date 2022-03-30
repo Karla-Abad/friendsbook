50fa52f9f3029b43f87aba300a0ca2c29b883e7c
Karla Abad <kabad1014@gmail.com>
2022-03-30 10:51:59 -0500

    Like and dislike
    Front end seems to be ready to go

--- client/src/components/share/Share.js
+++ client/src/components/share/Share.js
@@ -1,41 +1,57 @@
-import './share.css'
-import { PermMedia, Label, Room, EmojiEmotions } from '@material-ui/icons'
-
+import "./share.css";
+import { PermMedia, Label, Room, EmojiEmotions } from "@material-ui/icons";
+import { AuthContext } from "../../context/AuthContext";
+import { useState, useEffect, useContext } from "react";
 
 const Share = (props) => {
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+  const { user } = useContext(AuthContext);
 
-    const { username } = props
-    return (
-        <div className='share'>
-            <div className="shareWrapper">
-                <div className="shareTop">
-                    <img className='shareProfileImg' src='' alt="" />
-                    <input className='shareInput' placeholder="What's on your mind?" type="text" name="" />
-                </div>
-                <hr className="shareHr" />
-                <div className="shareBottom">
-                    <div className="shareOptions">
-                        <div className="shareOption">
-                            <PermMedia htmlColor='tomato' className='shareIcon' />
-                            <span className='shareOptionText'>Photo / Video</span>
-                        </div>
-                        <div className="shareOption">
-                            <Label htmlColor='blue' className='shareIcon' />
-                            <span className='shareOptionText'>Tag</span>
-                        </div>
-                        <div className="shareOption">
-                            <Room htmlColor='green' className='shareIcon' />
-                            <span className='shareOptionText'>Location</span>
-                        </div>
-                        <div className="shareOption">
-                            <EmojiEmotions htmlColor='gold' className='shareIcon' />
-                            <span className='shareOptionText'>Emojis</span>
-                        </div>
-                    </div>
-                    <button className='shareButton'>Share</button>
-                </div>
-            </div>
+  const { username } = props;
+  return (
+    <div className="share">
+      <div className="shareWrapper">
+        <div className="shareTop">
+          <img
+            className="shareProfileImg"
+            src={
+              user.profilePicture
+                ? PF + user.profilePicture
+                : PF + "users/noAvatar.png"
+            }
+            alt=""
+          />
+          <input
+            className="shareInput"
+            placeholder="What's on your mind?"
+            type="text"
+            name=""
+          />
         </div>
-    )
-}
-export default Share
+        <hr className="shareHr" />
+        <div className="shareBottom">
+          <div className="shareOptions">
+            <div className="shareOption">
+              <PermMedia htmlColor="tomato" className="shareIcon" />
+              <span className="shareOptionText">Photo / Video</span>
+            </div>
+            <div className="shareOption">
+              <Label htmlColor="blue" className="shareIcon" />
+              <span className="shareOptionText">Tag</span>
+            </div>
+            <div className="shareOption">
+              <Room htmlColor="green" className="shareIcon" />
+              <span className="shareOptionText">Location</span>
+            </div>
+            <div className="shareOption">
+              <EmojiEmotions htmlColor="gold" className="shareIcon" />
+              <span className="shareOptionText">Emojis</span>
+            </div>
+          </div>
+          <button className="shareButton">Share</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Share;
